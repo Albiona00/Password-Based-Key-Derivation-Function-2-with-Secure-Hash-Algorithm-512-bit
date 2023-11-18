a8d8c15ed73a57c075c60e9449aa1c63498cd2f9
Albiona00 <albiona.vukaj@student.uni-pr.edu>
2023-11-18 16:27:01 +0200

    added methods for generating salt and derivedkey

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,5 +18,21 @@ namespace PBKDF2_SHA512BIT
             InitializeComponent();
         }
 
+        static byte[] GenerateSalt(int saltLength = 16)
+        {
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] salt = new byte[saltLength];
+                rng.GetBytes(salt);
+                return salt;
+            }
+        }
+        static byte[] GenerateDerivedKey(string password, byte[] salt, int iterations, int derivedKeyLength)
+        {
+            using (Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA512))
+            {
+                return pbkdf2.GetBytes(derivedKeyLength);
+            }
+        }
     }
 }
