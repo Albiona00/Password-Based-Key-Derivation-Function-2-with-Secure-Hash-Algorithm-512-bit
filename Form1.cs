34adc3da25c608902ea60de4842c739ffbb5f242
Albiona00 <albiona.vukaj@student.uni-pr.edu>
2023-11-18 16:42:21 +0200

    added action event for generating  derived key when given username and passwords input

--- Form1.cs
+++ Form1.cs
@@ -23,6 +23,34 @@ namespace PBKDF2_SHA512BIT
             txt_username.Clear();
             txt_password.Clear();
         }
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string username = txt_username.Text;
+            string password = txt_password.Text;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                // Example parameters
+                byte[] salt = GenerateSalt();
+                int iterations = 1; // Number of iterations (adjust according to your security requirements)
+                int derivedKeyLength = 64; // 64 bytes for SHA-512
+
+                // Generate the derived key using PBKDF2-SHA512
+                byte[] derivedKey = GenerateDerivedKey(password, salt, iterations, derivedKeyLength);
+
+                // Convert the derived key to a hex string for storage or comparison
+                string derivedKeyHex = BitConverter.ToString(derivedKey).Replace("-", "");
+
+                // Now, you can use the derivedKeyHex as needed (e.g., compare it with stored hash)
+
+                // For testing purposes, you can display the derived key in a message box
+                MessageBox.Show("Login Successful!\nDerived Key (Hex): " + derivedKeyHex, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please enter both username and password.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
 
         private void button_exit_Click(object sender, EventArgs e)
         {
